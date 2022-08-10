ea03ed56850f1e3c39a0c6912492d660b0e65063
cyborg001 <cgrs27@gmail.com>
2022-08-10 22:45:07 +0000

    Modifica make_predict_azure_app.sh

--- tests/predictions.py
+++ tests/predictions.py
@@ -23,8 +23,11 @@ def predictions():
     }
 
     PORT=443
+    PORT=5000
 
     cabeceras = {'Content-Type': 'application/json'} 
-    r = requests.post(f'https://calm-forest-cdeb821ed6704dc19dd91c6e1388512b.azurewebsites.net:{PORT}/predict', headers=cabeceras, 
-                json=data)
+    # r = requests.post(f'https://calm-forest-cdeb821ed6704dc19dd91c6e1388512b.azurewebsites.net:{PORT}/predict', headers=cabeceras, 
+    #             json=data)
+    r = requests.post(f'https://127.0.0.1:{PORT}/predict', headers=cabeceras, 
+    #             json=data)
     return r.json()
