7c0cf0bdba0105bd8a02ae456d9faff7ce80959b
cyborg001 <cgrs27@gmail.com>
2022-08-11 04:40:04 +0000

    Modify test_predictions.py

--- tests/test_predictions.py
+++ tests/test_predictions.py
@@ -3,4 +3,4 @@ from . import predictions
 def test_predictions():
     result = predictions.my_prediction()
     print(result)
-    assert 'prediction' in result
+    assert 'prediction' not in result
