26e2ee19e85debd42037c53dcd9944793c1abb74
cyborg001 <cgrs27@gmail.com>
2022-08-12 06:47:47 +0000

    making some changes

--- tests/predictions.py
+++ tests/predictions.py
@@ -2,7 +2,7 @@ import subprocess
 
 def my_prediction():
     PIPE=subprocess.PIPE
-    prediction = subprocess.run("./make_prediction.sh",stdout=PIPE, stderr=PIPE)
+    prediction = subprocess.run("./make_predict_azure_app.sh",stdout=PIPE, stderr=PIPE)
     return str(prediction.stdout)
 
 
